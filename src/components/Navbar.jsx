7fb9e417b7f35a593cdd2cfcbadc529b502f9898
Joel Martinez <joematz@live.com>
2024-01-15 09:35:33 -0600

    Conditional renderin of user in process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  let [isToken, setIstoken] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
-  });
+
+    if (token) {
+      setIstoken(true);
+    }
+  }, []);
 
   function Login() {
     navigate("/Login");
@@ -18,6 +23,10 @@ export default function Navbar() {
 
   function Home() {
     navigate("/");
+  }
+
+  function Createpost() {
+    navigate("/newpost");
   }
 
   return (
@@ -35,19 +44,36 @@ export default function Navbar() {
         placeholder="Search..."
       />
 
-      <button
-        className="w-20 h-10 rounded-md  hover:bg-sky-100 hover:underline underline-offset-2 hover:text-blue-800 ml-auto"
-        onClick={Login}
-      >
-        Log in
-      </button>
+      {isToken && (
+        <>
+          <button
+            className="w-20 h-10 rounded-md  hover:bg-sky-100 hover:underline underline-offset-2 hover:text-blue-800 ml-auto"
+            onClick={Createpost}
+          >
+            Create post
+          </button>
 
-      <button
-        className="font-bold text-blue-600 w-36 h-10 border border-blue-600 rounded-md hover:bg-blue-700 hover:text-white hover:underline underline-offset-2 mr-32"
-        onClick={Createaccount}
-      >
-        Create Account
-      </button>
+          <img src="" alt="" className="rounded-full w-10 h-10 mr-32" />
+        </>
+      )}
+
+      {!isToken && (
+        <>
+          <button
+            className="w-20 h-10 rounded-md  hover:bg-sky-100 hover:underline underline-offset-2 hover:text-blue-800 ml-auto"
+            onClick={Login}
+          >
+            Log in
+          </button>
+
+          <button
+            className="font-bold text-blue-600 w-36 h-10 border border-blue-600 rounded-md hover:bg-blue-700 hover:text-white hover:underline underline-offset-2 mr-32"
+            onClick={Createaccount}
+          >
+            Create Account
+          </button>
+        </>
+      )}
     </nav>
   );
 }
